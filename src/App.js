243621d64ba49454404bca4fe86e1642d444c5e1
Manzar H Siddique <Manzar.Hussain@maersk.com>
2019-08-28 14:08:05 +0530

    install and used Radium styling (StyleRoot, Hover, media query)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 // import React, { useState } from 'react'; //Used for function based components
 import './App.css';
 import Person from './Person/Person';
+import Radium, { StyleRoot } from 'radium';
 
 
 //Below commneted one is class based component example
 class App extends Component {
   state = {
     persons: [
-      { id: "abdgd", name: "Manzar", age:30 },
-      { id: "kjskjkjd", name: "Hussain", age:31 },
-      { id: "idkjkjd", name: "Siddique", age:32 }
+      { id: "abdgd", name: "Manzar", age: 30 },
+      { id: "kjskjkjd", name: "Hussain", age: 31 },
+      { id: "idkjkjd", name: "Siddique", age: 32 }
     ],
     otherState: 'some other value',
     showPersons: false
@@ -30,113 +31,83 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({persons: persons});
+    this.setState({ persons: persons });
 
     // const person = Object.assign({}, this.state.persons[personIndex]); //this is the other way to do
-    // this.setState({
-    //   persons: [
-    //     { name:"Manzar", age:30 },
-    //     { name:event.target.value, age:29 },
-    //     { name:"Siddique", age:32 }
-    //   ]
-    // })
   }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
+    this.setState({ showPersons: !doesShow });
 
   }
 
   deletePersonhandler = (personIndex) => {
     const persons = this.state.persons;
     persons.splice(personIndex, 1);
-    this.setState({persons: persons});
+    this.setState({ persons: persons });
 
   }
 
   render() {
     const styleConst = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
       font: 'inherit',
       border: '1px solid blue',
-      padding:  '8px',
-      cursor: 'pointer'
+      padding: '8px',
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
-    
-    if(this.state.showPersons) {
+
+    if (this.state.showPersons) {
 
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person 
-                name={person.name} 
-                age={person.age}
-                key={person.id}
-                changed={(event) => this.nameChangedHandler(event, person.id)}
-                click={() => this.deletePersonhandler(index)}/>;
+            return <Person
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}
+              click={() => this.deletePersonhandler(index)} />;
           })}
 
-            {/*<Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age}/>
-
-            <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age} 
-              click={this.switchNameHandler.bind(this, "Manzar...!")}
-              changed={this.nameChangedHandler}> My Hobbies: Racing
-            </Person>
-
-            <Person 
-              name={this.state.persons[2].name} 
-              age={this.state.persons[2].age}/>*/}
-
-          </div> 
-      )
+        </div>
+      );
+      styleConst.backgroundColor = 'red';
+      styleConst[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I am React App</h1>
-        <p>This is really working!</p>
-        <button style={styleConst}
-        onClick={this.togglePersonsHandler}>Toggle Person </button>
-        {persons}
-        {/*{
-          this.state.showPersons === true ?
-          <div>
-            <Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age}/>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I am React App</h1>
+          <p>This is really working!</p>
+          <button style={styleConst}
+            onClick={this.togglePersonsHandler}>Toggle Person </button>
+          {persons}
 
-            <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age} 
-              click={this.switchNameHandler.bind(this, "Manzar...!")}
-              changed={this.nameChangedHandler}> My Hobbies: Racing
-            </Person>
 
-            <Person 
-              name={this.state.persons[2].name} 
-              age={this.state.persons[2].age}/>
-
-          </div> : null
-        }*/}
-        
-
-      </div>
+        </div>
+      </StyleRoot>
     );
     // return React.createElement('div', null, 'h1', 'Hi, I\'am React App!!!');
-                                          //hi will be interpretet as text not element
+    //hi will be interpretet as text not element
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'am React App!!!'));
-                                        //Adding css
+    //Adding css
   }
 }
 
-export default App;
+// export default App;
+export default Radium(App);
 
 //Function based componet example
 /*const app = props => {
