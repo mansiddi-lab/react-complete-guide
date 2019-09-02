4de22be09e32b98df4a4e2cabbc04451076509cf
Manzar H Siddique <Manzar.Hussain@maersk.com>
2019-09-02 12:38:51 +0530

    React.Fragment build in component to wrap other component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -8,7 +8,6 @@ class Person extends Component {
         console.log('[Person.js] persons rendering');
         return (
             /*<div className={classes.Person} >*/
-            // <Aux>
             <Fragment>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
@@ -17,7 +16,6 @@ class Person extends Component {
                     value={this.props.name} />
 
             </Fragment>
-            // </Aux>
             /*</div>*/
         );
     }
