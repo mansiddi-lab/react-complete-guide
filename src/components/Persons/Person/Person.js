bd4f309d94f25bdd7ffa738dce554f83933ca7cd
Manzar H Siddique <Manzar.Hussain@maersk.com>
2019-09-02 12:29:30 +0530

    using Higher order component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
+import Aux from '../../../hoc/Auxilary';
 
 class Person extends Component {
 
     render() {
         console.log('[Person.js] persons rendering');
         return (
-            <div className={classes.Person} >
+            <Aux>
+             {/*<div className={classes.Person} >*/}
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text"
                     onChange={this.props.changed}
                     value={this.props.name} />
-            </div>
+             {/*</div>*/}
+            </Aux>
         );
     }
 }
