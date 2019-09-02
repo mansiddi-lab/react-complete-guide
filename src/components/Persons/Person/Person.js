a538a63185691328103476084a84fa0027b3699a
Manzar H Siddique <Manzar.Hussain@maersk.com>
2019-09-02 12:36:59 +0530

    using react build in aux component instead of our created Auxilary component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import classes from './Person.css';
 import Aux from '../../../hoc/Auxilary';
 
@@ -7,15 +7,18 @@ class Person extends Component {
     render() {
         console.log('[Person.js] persons rendering');
         return (
-            <Aux>
-             {/*<div className={classes.Person} >*/}
+            /*<div className={classes.Person} >*/
+            // <Aux>
+            <Fragment>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text"
                     onChange={this.props.changed}
                     value={this.props.name} />
-             {/*</div>*/}
-            </Aux>
+
+            </Fragment>
+            // </Aux>
+            /*</div>*/
         );
     }
 }
